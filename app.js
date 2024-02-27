86fccc4acd8adaa13e12e35b2f7f6a9d8d1097b6
aungthuyeinhein <aungthuyeinhein98@gmail.com>
2024-02-27 16:09:23 +0630

    clean unnecessary module

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 //Core Module
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const MongoStore = require("connect-mongo");
 
 //User Define Module
 
@@ -18,19 +15,6 @@ Db();
 
 //passing the data using middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    store: MongoStore.create({
-      mongoUrl: process.env.MONGODB_URI,
-    }),
-    cookie: { maxAge: new Date(Date.now() + 3600000) },
-  })
-);
 
 // Mount image routes
 app.use("/images", imageRoutes, express.static("public/img"));
