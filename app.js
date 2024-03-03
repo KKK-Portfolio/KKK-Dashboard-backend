d6f3326f984ec78927ea02fe1723dd3609a81f78
aungthuyeinhein <aungthuyeinhein98@gmail.com>
2024-03-03 15:56:17 +0630

    initial setup

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ const express = require("express");
 
 //User Define Module
 
-const contactUsRoutes = require("./routes/contactUsRoutes");
-
 const Db = require("./config/dbConfig");
 
 const app = express();
@@ -17,8 +15,6 @@ Db();
 //passing the data using middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use("/contact-us", require("./routes/contactUsRoutes"));
 
 app.listen(PORT, () => {
   console.log(`Server is starting at : ${PORT}...`);
