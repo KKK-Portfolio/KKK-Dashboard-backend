809c0f4a95c8139e831c3a1512d5919fb7af96ad
aungthuyeinhein <aungthuyeinhein98@gmail.com>
2024-03-08 11:58:28 +0630

    revert: revert to one commit

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //Core Module
 const express = require("express");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
 
 //User Define Module
 
@@ -16,9 +14,8 @@ Db();
 
 //passing the data using middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use("/", require("./routes/userRoutes"));
-app.use("/", require("./routes/adminRoutes"));
 app.listen(PORT, () => {
   console.log(`Server is starting at : ${PORT}...`);
 });
