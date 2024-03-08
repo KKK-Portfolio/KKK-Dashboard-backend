03183ce44680e8e08dd8d9fb048a22bfdef35b4d
aungthuyeinhein <aungthuyeinhein98@gmail.com>
2024-03-08 12:06:41 +0630

    CORS added

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ const PORT = process.env.PORT || 3000;
 //connected to database
 Db();
 
+app.use(
+  cors({
+    origin: "*", // Allow requests from any origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specified HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow specified headers
+    credentials: true, // Allow cookies to be sent with requests
+  })
+);
+
 //passing the data using middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
