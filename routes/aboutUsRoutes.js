1f4c8f2ad92d3b16ab4006e118360bfb999ba356
aungthuyeinhein <aungthuyeinhein98@gmail.com>
2024-03-15 04:28:03 +0630

    feat: About Us CRUD coplete

--- routes/aboutUsRoutes.js
+++ routes/aboutUsRoutes.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const aboutUs = require("../controllers/aboutUsController");
 
 router = express.Router();
 
-router.post("upload");
+// router.post("/api/v1/AboutUs", aboutUs.createAboutUs);
+router.get("/api/v1/AboutUs", aboutUs.getAboutUs);
+router.put("/api/v1/:id", aboutUs.updateAboutUs);
+// router.delete("/api/v1/:id", aboutUs.deleteAboutUs);
+
+module.exports = router;
